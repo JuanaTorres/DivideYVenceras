c44c0a32b33566b20cfdb415624f5aca091f6423
Paula Anaya <panaya@unbosque.edu.co>
2021-10-15 16:49:29 -0500

    Funcion de multiplicacion de matrices

--- DivideYVenceras/src/co/edu/unbosque/model/Matriz.java
+++ DivideYVenceras/src/co/edu/unbosque/model/Matriz.java
@@ -48,14 +48,16 @@ public class Matriz {
 
 	}
 	
+	
 
 	public int[][] multiply(int[][] a, int[][] b) {
 		int[][] c = new int[a.length][b[0].length];
+		//Se valida si la matriz se puede multiplicar 
 		if (a[0].length == b.length) {
 			for (int i = 0; i < a.length; i++) {
 				for (int j = 0; j < b[0].length; j++) {
 					for (int k = 0; k < a[0].length; k++) {
-						// aquí se multiplica la matriz
+						// Se multiplica la matriz
 						c[i][j] += a[i][k] * b[k][j];
 					}
 				}
@@ -81,4 +83,48 @@ public class Matriz {
 		return num;
 	}
 	
+	
+	
+	//Metodos Strassen 
+	
+	
 }
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
